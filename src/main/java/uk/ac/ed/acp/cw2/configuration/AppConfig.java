b9fcbb249f6cb624fb995a3c5bebc4eff1b6a2c1
mglienecke <michael@glienecke.de>
2025-05-01 20:22:03 +0100

    dockerfile now builds the project as well

--- src/main/java/uk/ac/ed/acp/cw2/configuration/AppConfig.java
+++ src/main/java/uk/ac/ed/acp/cw2/configuration/AppConfig.java
@@ -1,11 +1,12 @@
 package uk.ac.ed.acp.cw2.configuration;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import uk.ac.ed.acp.cw2.data.RuntimeEnvironment;
 
 @Configuration
+@EnableScheduling
 public class AppConfig {
     @Bean
     public RuntimeEnvironment CurrentRuntimeEnvironment() {
